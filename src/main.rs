d7f7acd5137e42b931796b8195936a0062767098
Gabe Kamalakis <gabrielok98@gmail.com>
2019-10-19 13:05:49 -0400

    added termion deps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate tui;
+extern crate termion;
 use tui::Frame;
 use tui::layout;
 use tui::widgets;
 use tui::buffer;
 use tui::backend;
+use termion::screen;
+
 
 fn main() {
     println!("test")
