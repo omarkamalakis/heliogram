ee7286cb00ae20ae70ad887cd4c2016eb723952e
Gabe <gabrielok98@gmail.com>
2019-11-21 15:07:49 -0500

    got a window drawn

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
-extern crate tui;
-extern crate termion;
 #[allow(dead_code)]
 
 use std::io;
-use tui::Frame;
-use tui::layout;
-use tui::widgets;
-use tui::buffer;
+use tui::Terminal;
 use tui::backend::TermionBackend;
-use termion::*;
+use termion::raw::IntoRawMode;
+// use termion::*;
+use tui::widgets::{Text, Paragraph, Widget, Block, Borders};
+use tui::style::{Style, Color};
 
+// use tui::layout::{Layout, Constraint, Direction};
 
-fn main() {
-    println!("test")
+// Test window
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    // let mut counter = 0;
+    let text = [
+            Text::raw("Testing the paragraph engine")
+    ];
+    terminal.draw(|mut f| {
+            let size = f.size();
+            Block::default()
+                .title("Block")
+                .borders(Borders::ALL)
+                .render(&mut f, size);
+    //         Block::default()
+    //             .title("Blah")
+    //             .title_style(Style::default().fg(Color::Blue))
+    //             .borders(Borders::ALL)
+    //             .border_style(Style::default().fg(Color::Green))
+    //             .render(&mut f, size);
+    // });
+        Paragraph::new(text.iter())
+            .style(Style::default().fg(Color::Red));
+
+})
 }
 
-#[test]
-fn render() {
-   // let window = termion::;
-}
+
 
